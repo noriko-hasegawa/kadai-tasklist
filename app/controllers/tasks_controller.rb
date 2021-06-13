caed70cea58397521d096ae4e132a05a907ab745
noriko.hasegawa <pikadon_nh@yahoo.co.jp>
2021-06-13 12:52:02 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,11 +12,11 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @tasks = Task.new(task_params)
 
-    if @task.save
+    if @tasks.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to @tasks
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
@@ -24,15 +24,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @tasks = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    @tasks = Task.find(params[:id])
 
-    if @task.update(task_params)
+    if @tasks.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to @tasks
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
@@ -40,11 +40,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to messages_url
+    redirect_to tasks_url
   end
 
   private
